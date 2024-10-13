dd8a200b9d99e2cf95e0e91c842986aa8a4a3fa7
ochsner3k <40705326+ochsner3k@users.noreply.github.com>
2024-10-13 10:50:06 -0400

    Navbar + Footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,17 @@
-import { Box, Link, Image, Text, Button  } from "@chakra-ui/react";
+// Sign in/up code is commented out
+import { Box, Link, Text, Menu, MenuButton, MenuList, MenuItem, IconButton, useDisclosure } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
 import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import Signin from "./Signin";
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+// import Signin from "./Signin";
 import { useEffect, useState } from "react";
 
 
-function Navbar(){
-
+function Navbar() {
     const [isscroll, setisscroll] = useState(false);
+    const { isOpen, onOpen, onClose } = useDisclosure(); 
+
     function isScrolling() {
         if (window.scrollY > 80) {
             setisscroll(true);
@@ -17,48 +19,59 @@ function Navbar(){
             setisscroll(false);
         }
     }
+
     useEffect(() => {
         window.addEventListener("scroll", isScrolling);
         return () => {
-        window.removeEventListener("scroll");
-        }
-    }, [])
+            window.removeEventListener("scroll", isScrolling);
+        };
+    }, []);
 
-    
-    const [isAuth, setAuth]= useState(false);
+    // const [isAuth, setAuth]= useState(false);
 
-    return <Box w="full" boxShadow={ isscroll? 'md' : "sm"} position="fixed" bg="white" zIndex={1}  p="3px 0px">
+
+    return (
+        <Box w="full" boxShadow={isscroll ? 'md' : "sm"} position="fixed" bg="#FFFFFF" zIndex={1} p="3px 0px">
             <Box h="60px" w="85%" m="auto" display="flex" alignItems="center" justifyContent="space-between">
-            <Link to="/">
-            <Image boxSize='190px' src="https://static.tacdn.com/img2/brand_refresh/Tripadvisor_lockup_horizontal_secondary_registered.svg" alt="logo" />
-            </Link>
-            <Box display="flex" gap="25px" alignItems="center">
-                <Link display="flex">
-                    <ModeEditOutlineOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Review</Text>
+                <Link href="/">
+                    <img src="/otg.png" alt="pls show D:" style={{ width: '175px', height: '60px' }} />
+                    </Link> 
+                        <Box display="flex" gap="25px" alignItems="center">
+                    <Link display="flex">
+                        <ModeEditOutlineOutlinedIcon />
+                        <Text fontWeight="500" fontSize='md'>Create a Post</Text>
+                    </Link>
+                    <Link display="flex" alignItems="center" gap="5px">
+                        <MenuBookIcon />
+
+                        <Text fontWeight="500" fontSize='md'>My Planner</Text>
                 </Link>
-                <Link display="flex">
-                    <FavoriteBorderOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Trips</Text>
-                </Link>
-                <Link display="flex">
-                    <NotificationsOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Alerts</Text>
-                </Link>
-                <Link display="flex">
+                    
+                    
+                <Menu isOpen={isOpen}>
+                    <MenuButton
+                        as={IconButton}
+                        icon={<PersonOutlineOutlinedIcon />}
+                        variant="ghost"
+                        onMouseEnter={onOpen} 
+                        onMouseLeave={onClose} 
+                    />
+                    <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
+                        <MenuItem>Account Info</MenuItem>
+                        <MenuItem>Bookmarks</MenuItem>
+                    </MenuList>
+                </Menu> 
+                {/* <Link display="flex">
                     {isAuth ? 
                     <Button color="black" onClick={()=>{
                         setAuth(!isAuth);
                     }}>Sanghamitra</Button> : <Signin />
                 }
-                </Link>
-                <Link display="flex">
-                    <ShoppingCartOutlinedIcon />
-                    <Text fontWeight="500" fontSize='md'>Basket</Text>
-                </Link>
+                </Link>  */}
+                </Box>
             </Box>
         </Box>
-    </Box>
+    );
 }
 
 export default Navbar;
