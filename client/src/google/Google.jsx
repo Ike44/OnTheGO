f28aeebc6a7cd186cbb58a7229c6d5a02b4cd147
David Taylor <dtayl111@uncc.edu>
2024-10-23 18:19:04 -0400

    add .env file update

--- client/src/google/Google.jsx
+++ client/src/google/Google.jsx
@@ -1,10 +1,8 @@
-import { GOOGLE_API_KEY} from './config';
-
 let autocompleteService = null;
 
 export function initGoogleMapsAPI() {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&libraries=places`;
     script.async = true;
     script.defer = true;
     script.onload = () => {
--- client/src/google/config.js
+++
@@ -1,1 +0,0 @@
-export const GOOGLE_API_KEY = 'AIzaSyByptjYkJJ_22nalxS2y1ww9RcqyYrejWQ';
